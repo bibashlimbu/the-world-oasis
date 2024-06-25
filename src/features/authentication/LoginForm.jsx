2d715d13591322b7402e93088a9e6883547187ba
bibashlimbu <bibas9892@gmail.com>
2024-06-25 22:39:26 +0545

    email and pass for login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("pihox19841@dalebig.com");
+  const [password, setPassword] = useState("33333333");
 
   const { login, isLoading } = useLogin();
 
